b1a0a8fcd729f0fe76484b4c70ae75d71d3da028
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-08 22:10:08 -0600

    Making changes

--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 $link = new mysqli("itmo544aravinddb.czw4txekmxpp.us-west-2.rds.amazonaws.com","aravind","password","ITMO544AravindDb",3306) or die("Error " . mysqli_error($link));
-echo "Successfully connected to Database....";
+echo "Successfully connected to Database..";
 $result=$link->query("CREATE TABLE MP1(
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 uname VARCHAR(20),
@@ -11,7 +11,7 @@ FinishedS3URL VARCHAR(256),
 jpegfilename VARCHAR(256),
 state TinyInt(3) CHECK (state IN (0,1,2)),
 DateTime Timestamp)");
-echo "\nTable Created/Present....";
+echo "\nTable Created/Present..";
 shell_exec("chmod 600 setup.php");
-echo "\nFile permission successfully modified....\n";
+echo "\nFile permission successfully modified..\n";
 ?>
